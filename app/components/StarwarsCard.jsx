fb69117bbc8f3f3bb7339c30d9b5e70f6f971865
Nicholas Pritchyk <nickpritchyk20@gmail.com>
2023-07-24 12:34:53 -0600

    fixed grid view on medium screen size

--- app/components/StarwarsCard.jsx
+++ app/components/StarwarsCard.jsx
@@ -18,7 +18,7 @@ function StarwarsCard({listView, people, films, setIsOpen, setPeopleIdx, setCard
 
   return (
         // className changed based on list or grid view state 
-    <div className={listView ? 'flex flex-col' : 'md:grid md:grid-cols-3 md:gap-4 md:w-screen'}>
+    <div className={listView ? 'flex flex-col' : 'sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 md:gap-4 md:w-screen'}>
             {people.length > 0 && people.map((res, idx) => 
                 <div key={idx} className='relative flex flex-col items-center space-y-4 p-2 m-12 text-center justify-center justify-self-center hover:scale-[1.02] rounded-md shadow-lg border-[0.2px] border-blue-400 w-52 h-64 lg:w-80 lg:h-80'>
                     <div className="w-full absolute top-0">
